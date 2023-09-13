d58e2aaa83ba624ffcd7de65cbc8557305a1cac3
Khang Tran <khangtrann8198@gmail.com>
2023-09-13 11:18:15 +0000

    Config environment variables

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { enableProdMode } from '@angular/core';
+
+if (process.env['NODE_ENV'] === 'production') {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
