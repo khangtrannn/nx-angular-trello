70bdd9b44aa1b0c5b32604625219367dd2735bb8
Khang Tran <khangtrann8198@gmail.com>
2023-09-14 10:30:54 +0000

    Add dummy task card

--- modules/project-board/ui/task-list/src/lib/task-list/task-list.component.ts
+++ modules/project-board/ui/task-list/src/lib/task-list/task-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
+import { TaskCardComponent } from 'project-board/ui/task-card';
 
 @Component({
   selector: 'trello-task-list',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, TaskCardComponent],
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.scss'],
 })
