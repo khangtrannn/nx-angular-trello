6654c93cec1d40e2bfd6833731060c0d3f4f46c4
Khang Tran <khangtrann8198@gmail.com>
2023-09-19 04:26:42 +0000

    Refactor to use cdkDropListGroup

--- modules/project-board/ui/project-board-canvas/src/lib/project-board-canvas/project-board-canvas.component.ts
+++ modules/project-board/ui/project-board-canvas/src/lib/project-board-canvas/project-board-canvas.component.ts
@@ -1,22 +1,30 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TaskList } from 'api-interfaces';
+import { Task, TaskList } from 'api-interfaces';
 import { TaskListComponent } from 'project-board/ui/task-list';
 import { ListCreatorComponent } from 'modules/project-board/ui/list-creator/src';
+import { CdkDragDrop, CdkDropList, CdkDropListGroup, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'trello-project-board-canvas',
   standalone: true,
-  imports: [CommonModule, TaskListComponent, ListCreatorComponent],
+  imports: [CommonModule, CdkDropListGroup, CdkDropList, TaskListComponent, ListCreatorComponent],
   templateUrl: './project-board-canvas.component.html',
   styleUrls: ['./project-board-canvas.component.scss'],
 })
-export class ProjectBoardCanvasComponent implements OnInit {
+export class ProjectBoardCanvasComponent {
   @Input() taskLists!: TaskList[];
 
-  listIds: string[] = [];
-
-  ngOnInit(): void {
-    this.listIds = this.taskLists.reduce((acc: string[], taskList: TaskList) => ([...acc, taskList.id]), []);
+  drop(event: CdkDragDrop<Task[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 }
