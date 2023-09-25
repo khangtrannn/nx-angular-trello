016b9cd52b462c467d138d278792e089967205fb
Khang Tran <khangtrann8198@gmail.com>
2023-09-25 05:01:03 +0000

    Init editor dialog

---
+++ modules/task-editor/ui/editor-dialog/README.md
@@ -0,0 +1,7 @@
+# editor-dialog
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test editor-dialog` to execute the unit tests.
---
+++ modules/task-editor/ui/editor-dialog/src/lib/editor-dialog/editor-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'trello-editor-dialog',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './editor-dialog.component.html',
+  styleUrls: ['./editor-dialog.component.scss'],
+})
+export class EditorDialogComponent {}
