bf8aa2afa433fd1a55f0090bf6d0f83aece05e11
Khang Tran <khangtrann8198@gmail.com>
2023-09-26 07:22:40 +0000

    Add task creator textarea

---
+++ modules/project-board/ui/task-creator/src/lib/task-creator-textarea/task-creator-textarea.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'trello-task-creator-textarea',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './task-creator-textarea.component.html',
+  styleUrls: ['./task-creator-textarea.component.scss'],
+})
+export class TaskCreatorTextareaComponent {}
--- modules/project-board/ui/task-creator/src/lib/task-creator/task-creator.component.ts
+++ modules/project-board/ui/task-creator/src/lib/task-creator/task-creator.component.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TaskCreatorTextareaComponent } from '../task-creator-textarea/task-creator-textarea.component';
 
 @Component({
   selector: 'trello-task-creator',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TaskCreatorTextareaComponent],
   templateUrl: './task-creator.component.html',
   styleUrls: ['./task-creator.component.scss'],
 })
-export class TaskCreatorComponent {}
+export class TaskCreatorComponent {
+  textareaMode = false;
+
+  toggleTextareaMode(): void {
+    this.textareaMode = !this.textareaMode;
+  }
+}
